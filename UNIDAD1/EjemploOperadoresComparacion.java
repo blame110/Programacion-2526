ab54a2d86da186579dc0882ca2468822796a89f9
pablo galvan <vgalflo309@g.educaand.es>
2025-09-23 14:02:21 +0200

    Condicionales

--- UNIDAD1/EjemploOperadoresComparacion.java
+++ UNIDAD1/EjemploOperadoresComparacion.java
@@ -3,25 +3,59 @@ package UNIDAD1;
 import java.util.Scanner;
 
 public class EjemploOperadoresComparacion {
-    
+
     public static void main(String[] args) {
-        
+
         boolean puedeIrDelante = false;
 
-        int altura = 0;
+        // En una linea se pueden declarar varias variables del mismo tipo
+        int edad = 0, altura = 0;
 
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Introduce la altura del niño: ");
-        //Con esta instruccion leemos un valor entero 
-        //de la terminal y se lo asignamos a la variable altura
-        altura= teclado.nextInt();
+        // Con esta instruccion leemos un valor entero
+        // de la terminal y se lo asignamos a la variable altur
+        altura = teclado.nextInt();
 
-        puedeIrDelante = altura >= 135;
+        System.out.print("Introduce la edad del niño: ");
+        edad = teclado.nextInt();
 
-        System.out.println("La condicion puede ir delante es " + puedeIrDelante);
+        // La estructura if valora si es cierta o falsa la condicion, en caso de ser
+        // cierta
+        // Entrara y ejecutara el codigo que tiene entre llaves
+        if (altura >= 135 || edad >= 10) {
+            System.out.println("El niño puede ir delante");
+        }
 
-     teclado.close();
+        // Icaro para poder volar desde la isla de creta hasta atenas necesitaba
+        // Pesar menos de 80.5 kg y cumplir una de las siguientes dos condiciones
+        // Hacer caso a su padre(boolean) o tener mas de rango 5 en google fit(int)
+        float peso = 87;
+        int googleFitLvl = 6;
+        boolean haceCasoFather = false;
+
+        /*
+         * Por defecto en una condicion se hace primero el Y && antes que el O
+         * En este ejemplo sino ponemos los parentesis primero haria el && y al final
+         * el O de google fit, por lo cual sólo con que cumpla la condicion de googlefit
+         * ya seria cierto el if, al poner parentesis forzamos a que primero haga
+         * el O y cumpla la lógica del enunciado
+         */
+        if (peso < 80.5 && (haceCasoFather == true || googleFitLvl > 5))
+            System.out.println("Consigue Volar Hasta Atenas! Yuhuuuu");
+
+        // Para volver a por las llaves que se le habían olvidado en el laberinto
+        // Tiene que hacer caso a su padre y su peso sea distinto de 90
+        // o solo con tener un nivel google Fit mayor a 10 también valdría
+        if ((haceCasoFather && peso != 90) || googleFitLvl > 10)
+            System.out.println(
+                    "Estas alelao pero pudiste volver a por las llaves\n P.d.Que sepas que las alas ya se han fundido y estas atrapado de nuevo");
+
+        if (!haceCasoFather)
+            System.out.println("Dedadole le mete una toba");
+
+        teclado.close();
     }
 
 }
