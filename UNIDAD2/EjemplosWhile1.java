e2abde97abef13a653f23347a7a9b94f560b5736
pablo galvan <vgalflo309@g.educaand.es>
2025-10-07 13:01:42 +0200

    Todos los colaboradores
    Co-authored-by: warone11 <warone11@users.noreply.github.com>
    Co-authored-by: Mauriileon <Mauriileon@users.noreply.github.com>
    Co-authored-by: Jesús Palma González <xYisuhDev@users.noreply.github.com>
    Co-authored-by: ayoubrb5 <ayoubrb5@users.noreply.github.com>
    Co-authored-by: Azahara CM <azcama@users.noreply.github.com>
    Co-authored-by: Blas968 <Blas968@users.noreply.github.com>

--- UNIDAD2/EjemplosWhile1.java
+++ UNIDAD2/EjemplosWhile1.java
@@ -28,28 +28,45 @@ public class EjemplosWhile1 {
         }
 
         /*
-         * Ejercicio: leer las edades de 10 personas, que deben de
-         * generarse aleatoriamente entre 18 y 65, se debe de sacar por pantalla la edad
+         * Ejerciciov2: leer las edades de 10 personas, que deben de
+         * estar entre 18 y 65, se debe de sacar por pantalla la edad y si esta jubilado
+         * o si es menor de edad
          * y al final calcular la media
          */
 
         int edad = 0;
         int sumaEdad = 0;
+        int cantidadPersonas = 0;
         i = 1;
-
+        String mensaje = "";
         // Quiero que se repita 10 veces
         while (i <= 10) {
-            // Genero la edad aleatoriamente
-            edad = 18 + (int) (Math.random() * 48);
+            // Borramos el mensaje de la anterior repeticion
+            mensaje = "";
+            // Genero la edad aleatoriamente entre 1 y 100
+            edad = (int) (Math.random() * 100) + 1;
+
+            if (edad < 18)
+                mensaje = "La persona es menor de edad";
+            else if (edad > 65)
+                mensaje = "Esta jubilado";
+            else {
+
+                // Incrementamos la edad con la edad de la persona actual
+                sumaEdad = sumaEdad + edad;
+                // Incrementamos la cantidad de personas entre 18 y 65 para hacer la media
+                cantidadPersonas = cantidadPersonas + 1;
+            }
+
             // Mostramos la edad del personaje
-            System.out.println("La edad de la persona " + i + " es: " + edad);
+            mensaje = "La edad de la persona " + i + " es: " + edad + ". " + mensaje;
+            System.out.println(mensaje);
             // Pasamos a la siguiente persona
             i++;
-            sumaEdad = sumaEdad + edad;
-
         }
         // Calculalmos la media
-        sumaEdad = sumaEdad / 10;
+        if (cantidadPersonas != 0)
+            sumaEdad = sumaEdad / cantidadPersonas;
         System.out.println("La Media es: " + sumaEdad);
 
     }
