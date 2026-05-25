1dbf18610dcc639c2f7fb8f2675cc21dc34154f8
pablo galvan <vgalflo309@g.educaand.es>
2026-05-25 13:59:35 +0200

    SpringBoot

---
+++ SpringBoot/src/main/java/com/daw/springboot/Controller/CocheController.java
@@ -0,0 +1,30 @@
+package com.daw.springboot.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.daw.springboot.model.Coche;
+import com.daw.springboot.model.CocheRepository;
+
+@Controller
+public class CocheController {
+
+    @Autowired
+    public CocheRepository cocheRepository;
+
+    @GetMapping("/")
+    public String getCoches(Model model) {
+        // Cargamos todos los coches de la bd con findall
+        List<Coche> listaCoches = cocheRepository.findAll();
+        // Añadimos los datos al modelo para que le lleguen a la vista
+        model.addAttribute("listacoches", listaCoches);
+
+        return "listado";
+    }
+
+}
---
+++ SpringBoot/src/main/java/com/daw/springboot/model/CocheRepository.java
@@ -0,0 +1,7 @@
+package com.daw.springboot.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CocheRepository extends JpaRepository<Coche, Long> {
+
+}
